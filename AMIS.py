b1d5d7ac656034cba5ec38a13b8676a2c228b899
mispython <mis.saspython@gmail.com>
2025-12-01 14:31:04 +0800

    Update AMIS.py

--- AMIS.py
+++ AMIS.py
@@ -1,22 +1,92 @@
->>> READING TODAY'S DATA
-30                       ) if parquet_exists(TODAY_CHANNEL_SUM) else pl.DataFrame()
-31          try:
-32              return pq.read_schema(str(path)) is not None
-33      today_channel = (pl.read_parquet(TODAY_CHANNEL_SUM)
-34                       .rename({"PROMPT": "TOLPROMPT", "UPDATED": "TOLUPDATE"})
-35                       .select(["CHANNEL", "TOLPROMPT", "TOLUPDATE"])
-36                       .with_columns(pl.lit(REPTDATE.strftime("%b%y").upper()).alias("MONTH"))
-37                      ) if parquet_exists(TODAY_CHANNEL_UPDATE) else pl.DataFrame()
-38      today_update = (pl.read_parquet(TODAY_CHANNEL_UPDATE)
-39                          pl.when(pl.col("index") == 1).then("TOTAL PROMPT BASE")
-40                           .when(pl.col("index") == 2).then("TOTAL UPDATED")
-41                           .alias("LINE")
-Traceback (most recent call last):
-  File "/sas/pythonITD/mis/job/LOAN/EIBMCHNL.py", line 97, in <module>
-    today_update = (pl.read_parquet(TODAY_CHANNEL_UPDATE)
-  File "/sas/pythonITD/mis/lib64/python3.9/site-packages/polars/dataframe/frame.py", line 9141, in with_columns
-    return self.lazy().with_columns(*exprs, **named_exprs).collect(_eager=True)
-  File "/sas/pythonITD/mis/lib64/python3.9/site-packages/polars/lazyframe/frame.py", line 2032, in collect
-    return wrap_df(ldf.collect(callback))
-polars.exceptions.ColumnNotFoundError: TOTAL PROMPT BASE
-SAS Connection terminated. Subprocess id was 3899207
+DATA REPTDATE;
+   REPTDATE=TODAY()-1;
+   CALL SYMPUT('REPTYEAR', PUT(REPTDATE, YEAR2.));
+   CALL SYMPUT('REPTMON', PUT(MONTH(REPTDATE), Z2.));
+   CALL SYMPUT('REPTDAY', PUT(DAY(REPTDATE), Z2.));
+   CALL SYMPUT('RDATE', PUT(REPTDATE, DDMMYY8.));
+RUN;
+
+DATA CHANNEL(DROP=REPTDATE);
+   INFILE CHANNEL FIRSTOBS=1;
+   INPUT @001  CHANNEL    $UPCASE5.      /* CHANNEL                */
+         @006  TOLPROMPT         8.      /* TOTAL PROMPTED         */
+         @014  TOLUPDATE         8.      /* TOTAL UPDATED          */
+         ;
+         REPTDATE=TODAY()-2;
+         MONTH=PUT(REPTDATE,MONYY5.);
+RUN;
+PROC PRINT DATA=CHANNEL;RUN;
+
+PROC APPEND DATA=CHANNEL BASE=CRMWH.CHANNEL_SUM FORCE;RUN;
+PROC PRINT DATA=CRMWH.CHANNEL_SUM;RUN;
+
+DATA BRANCH;
+   INFILE BRANCH FIRSTOBS=1;
+   INPUT @001  BRANCHNO          5.      /* BRANCH NUMBER          */
+         @006  TOLPROMPT         8.      /* TOTAL PROMPTED         */
+         @014  TOLUPDATE         8.      /* TOTAL UPDATED          */
+         ;
+RUN;
+
+DATA BCODE;
+   INFILE BCODE;
+   INPUT @002   BRANCHNO       3.        /* BRANCH NUMBER          */
+         ;
+RUN;
+
+PROC SORT DATA=BCODE;BY BRANCHNO;RUN;
+PROC SORT DATA=BRANCH;BY BRANCHNO;RUN;
+
+DATA CRMWH.OTC_DETAIL_&REPTMON&REPTYEAR;
+   MERGE BCODE(IN=A) BRANCH(IN=B);BY BRANCHNO;
+   IF TOLPROMPT=. THEN DO TOLPROMPT=0;END;
+   IF TOLUPDATE=. THEN DO TOLUPDATE=0;END;
+   IF A;
+RUN;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+DATA REPTDATE;
+   REPTDATE=TODAY()-1;
+   CALL SYMPUT('REPTYEAR', PUT(REPTDATE, YEAR2.));
+   CALL SYMPUT('REPTMON', PUT(MONTH(REPTDATE), Z2.));
+   CALL SYMPUT('REPTDAY', PUT(DAY(REPTDATE), Z2.));
+   CALL SYMPUT('RDATE', PUT(REPTDATE, DDMMYY8.));
+RUN;
+
+     DATA SUM;
+        INFILE CHANNEL;
+        INPUT    @022 ATM      10.
+                 @037 EBK      10.
+                 @052 OTC      10.
+                 @067 TOTAL    10.
+                 @080 DATE    $10.
+                 ;
+     RUN;
+     DATA UAT(KEEP=DESC ATM EBK OTC TOTAL DATE);
+      SET SUM;
+      IF _N_ = 1 THEN DESC='TOTAL PROMPT BASE';
+      IF _N_ = 2 THEN DESC='TOTAL UPDATED';
+     RUN;
+     PROC PRINT DATA=UAT; RUN;
+     *;
+    PROC SQL;
+    CREATE TABLE UAT AS SELECT
+    DESC,ATM,EBK,OTC,TOTAL,DATE
+    FROM UAT;
+    QUIT;
+     *;
+     PROC APPEND DATA=UAT BASE=CHN.CHANNEL_UPDATE FORCE; RUN;
+     *;
