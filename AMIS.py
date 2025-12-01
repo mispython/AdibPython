731c7658f573e18761696f67db5680e6f0d45210
mispython <mis.saspython@gmail.com>
2025-12-01 10:34:14 +0800

    Update AMIS.py

--- AMIS.py
+++ AMIS.py
@@ -1,5 +1,9 @@
-input @2   BRCODE        8.
-      @6   BRABBR        $3.
-      @12  BRSTAT        $29.
-      @45  BRSTATEIND    $1.
-      @50  BRSTATUS      $1.;
+LRECL = 80
+
+LAYOUT = {'BRCODE': {'type': 'packed', 'slice': slice(1, 9, None)},
+ 'BRABBR': {'type': 'ebcdic', 'slice': slice(5, 8, None)},
+ 'BRSTAT': {'type': 'ebcdic', 'slice': slice(11, 40, None)},
+ 'BRSTATEIND': {'type': 'ebcdic', 'slice': slice(44, 45, None)},
+ 'BRSTATUS': {'type': 'ebcdic', 'slice': slice(49, 50, None)}}
+
+/sasdata/rawdata/lookup/LKP_BRANCH
