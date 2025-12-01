ba326cb2d49804ed64969eadfd1899f31ffb6be0
mispython <mis.saspython@gmail.com>
2025-12-01 15:30:19 +0800

    Update AMIS.py

--- AMIS.py
+++ AMIS.py
@@ -1,20 +1,17 @@
-B001 PCS   BANK-ATMC                             C                              
-B002 JSS   JALAN SULTAN SULAIMAN            W    O                              
-B003 JRC   JALAN RAJA CHULAN                W    O                              
-B004 MLK   MELAKA                           M    O                              
-B005 IMO   IPOH MAIN OFFICE                 A    O                              
-B006 PPG   PULAU PINANG                     P    O                              
-B007 JBU   JOHOR BAHRU                      J    O                              
-B008 KTN   KUANTAN                          C    O                              
-B009 JYK   JALAN YANG KALSOM                A    O                              
-B010 ASR   ALOR SETAR                       K    O                              
-B011 GRN   GURUN                            K    O                              
-B012 PPH   PASIR PUTEH                      D    C                              
-B013 KBU   KOTA BAHRU                       D    O                              
-B014 TMH   TANAH MERAH                      D    O                              
-B015 KPG   KEPONG                           W    O                              
-B016 NLI   NILAI                            N    O                              
-B017 TPN   TAMPIN                           N    O                              
-B018 PJN   PJ NEW TOWN                      B    O                              
-B019 DUA   DAMASARA UTAMA                   B    O                              
-B020 TCL   TAMAN CHI LIUNG                  B    O                              
+┌─────────┬────────┬─────────┐
+│ CHANNEL ┆ PROMPT ┆ UPDATED │
+│ ---     ┆ ---    ┆ ---     │
+│ str     ┆ i64    ┆ i64     │
+╞═════════╪════════╪═════════╡
+│ 00002   ┆ 16     ┆ 16      │
+│ 00003   ┆ 13     ┆ 13      │
+│ 00004   ┆ 70     ┆ 70      │
+│ 00005   ┆ 49     ┆ 49      │
+│ 00006   ┆ 55     ┆ 55      │
+│ …       ┆ …      ┆ …       │
+│ 00296   ┆ 28     ┆ 28      │
+│ 00701   ┆ 9      ┆ 9       │
+│ 00702   ┆ 8      ┆ 8       │
+│ 00703   ┆ 20     ┆ 20      │
+│ 00704   ┆ 53     ┆ 53      │
+└─────────┴────────┴─────────┘
