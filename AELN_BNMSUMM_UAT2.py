c20157c9cb33ff7bbded71eb3831af0882fb62fe
mispython <mis.saspython@gmail.com>
2025-12-05 15:25:52 +0800

    Update AELN_BNMSUMM_UAT2.py

--- AELN_BNMSUMM_UAT2.py
+++ AELN_BNMSUMM_UAT2.py
@@ -1,8 +1,41 @@
-2025-12-05 15:23:52,626 - INFO - Initialized combined processor for batch date: 20251204
-2025-12-05 15:23:52,626 - INFO - Starting combined loan data processing...
-2025-12-05 15:23:52,626 - INFO - Step 1: Processing input text file...
-2025-12-05 15:23:52,626 - INFO - Processing text file: /sasdata/rawdata/ln/R6999_BAL_ITEM_20251204.txt
-2025-12-05 15:23:55,211 - INFO - Read 2390784 lines from text file
-2025-12-05 15:24:02,113 - INFO - Processed 0 records from text file
-2025-12-05 15:24:02,330 - ERROR - No records processed from text file
-2025-12-05 15:24:02,331 - ERROR - Processing failed - check logs for details
+1033         PUBLIC BANK BERHAD                  LOAN ACCOUNTING      CONTROL TOTALS            R-6999-008-001 30-11-25   PAGE      1                                                                   
+0TOTALS  ACCRUAL DATE: 2025/11  COST CENTER: 0000012  FEE CATEGORY:   LOAN FEE PLAN:   LOAN TYPE: 205                                                                                                   
+0PRINCIPAL/LOAN BALANCE   *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0PRINCIPAL IN NON-ACCRUAL *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0INTEREST ACCRUALS        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0INTEREST IN NON-ACCRUAL                       TOTAL LOANS                              PARTS                   BANKS SHARE                                                                             
+ = TOTAL ACCRUAL                                         0.0049482-                         0.0000000                     0.0049482-                                                                    
+0UNEARNED INTEREST        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0RESERVE FINANCED         *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0NON DDA ESCROW FUNDS     *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0LATE FEES                *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0OTHER EARNED FEES        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0EXTENSION FEES           *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+1033         PUBLIC BANK BERHAD                  LOAN ACCOUNTING      CONTROL TOTALS            R-6999-008-001 30-11-25   PAGE      2                                                                   
+1033         PUBLIC BANK BERHAD                  LOAN ACCOUNTING      CONTROL TOTALS            R-6999-008-001 30-11-25   PAGE      3                                                                   
+0TOTALS  ACCRUAL DATE: 2025/11  COST CENTER: 0000012  FEE CATEGORY:   LOAN FEE PLAN:   LOAN TYPE: 320                                                                                                   
+0PRINCIPAL/LOAN BALANCE   *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0PRINCIPAL IN NON-ACCRUAL *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0INTEREST ACCRUALS        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0INTEREST IN NON-ACCRUAL                       TOTAL LOANS                              PARTS                   BANKS SHARE                                                                             
+ = TOTAL ACCRUAL                                         0.0041375                          0.0000000                     0.0041375                                                                     
+0UNEARNED INTEREST        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0RESERVE FINANCED         *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0NON DDA ESCROW FUNDS     *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0LATE FEES                *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0OTHER EARNED FEES        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0EXTENSION FEES           *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+1033         PUBLIC BANK BERHAD                  LOAN ACCOUNTING      CONTROL TOTALS            R-6999-008-001 30-11-25   PAGE      4                                                                   
+1033         PUBLIC BANK BERHAD                  LOAN ACCOUNTING      CONTROL TOTALS            R-6999-008-001 30-11-25   PAGE      5                                                                   
+0TOTALS  ACCRUAL DATE: 2025/11  COST CENTER: 0000012  FEE CATEGORY:   LOAN FEE PLAN:   LOAN TYPE: 521                                                                                                   
+0PRINCIPAL/LOAN BALANCE   *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0PRINCIPAL IN NON-ACCRUAL *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0INTEREST ACCRUALS        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0INTEREST IN NON-ACCRUAL                       TOTAL LOANS                              PARTS                   BANKS SHARE                                                                             
+ = TOTAL ACCRUAL                                         0.0046679-                         0.0000000                     0.0046679-                                                                    
+0UNEARNED INTEREST        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0RESERVE FINANCED         *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0NON DDA ESCROW FUNDS     *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0LATE FEES                *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0OTHER EARNED FEES        *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                                                                              
+0EXTENSION FEES           *  *  *  ALL  AMOUNTS  IN  THIS  DETAIL  CATEGORY  ARE  EQUAL  TO  ZERO  *  *  *                                 
