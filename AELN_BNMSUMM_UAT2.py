5329489663d7fdee2d95f3a482f66a943bfaad4e
mispython <mis.saspython@gmail.com>
2025-12-09 16:50:04 +0800

    Update AELN_BNMSUMM_UAT2.py

--- AELN_BNMSUMM_UAT2.py
+++ AELN_BNMSUMM_UAT2.py
@@ -11,3 +11,12 @@ Traceback (most recent call last):
   File "/sas/python/virt_edw/Data_Warehouse/MIS/Job/EIBWSIBC_SIB.py", line 92, in process_eln1
     pl.col('raw_line').str.slice(0, 13).str.strip().alias('AANO'),
 AttributeError: 'ExprStringNameSpace' object has no attribute 'strip'
+
+
+
+B001 PCS   BANK-ATMC                             C                              
+B002 JSS   JALAN SULTAN SULAIMAN            W    O                              
+B003 JRC   JALAN RAJA CHULAN                W    O                              
+B004 MLK   MELAKA                           M    O                              
+B005 IMO   IPOH MAIN OFFICE                 A    O                              
+B006 PPG   PULAU PINANG                     P    O                              
