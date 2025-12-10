9109067857a170ddd933fa08521a837375c4fcf4
mispython <mis.saspython@gmail.com>
2025-12-10 10:43:34 +0800

    Update AELN_BNMSUMM_UAT2.py

--- AELN_BNMSUMM_UAT2.py
+++ AELN_BNMSUMM_UAT2.py
@@ -1,34 +1,6 @@
-*;
-DATA SDBMS(DROP=OPENDD OPENMM OPENYY);
-  INFILE SDBMS FIRSTOBS=2;
-  INPUT @001 BRANCH       $3.
-        @005 NAME        $50.
-        @056 IC          $20.
-        @077 NATIONALITY  $2.
-        @079 BOXNO       $10.     /* SDB BOX NO.     */
-        @090 HIRERTY      $1.     /* HIRER TYPE      */
-        @092 ADDRESS    $250.
-        @343 ACCTHOLDER   $1.
-        @345 ACCTNO      $20.
-        @366 PRIPHONE    $20.
-        @387 MOBILENO    $20.
-        @408 OPENDD       $2.
-        @410 OPENMM       $2.
-        @412 OPENYY       $4.
-        @419 RENTALDATE   DDMMYY8. /* RENTALDUEDATE     */
-        @428 LASTRENTPAY  DDMMYY8. /* LASTRENTALPAYMENT */
-        @437 MTHOVERDUE1   2.      /* MTHOVERDUE(3-<4)  */
-        @439 MTHOVERDUE2   2.      /* MTHOVERDUE(4-<8)  */
-        @441 MTHOVERDUE3   2.      /* MTHOVERDUE(>8)    */
-        @443 TOTALOVERDUE  2.
-        ;
-  OPENDT = MDY(OPENMM,OPENDD,OPENYY);
-RUN;
-PROC PRINT DATA=SDBMS;RUN;
-*;
-DATA STATUS.SDB&REPTMON&NOWK&REPTYEAR   (DROP=NAME ADDRESS PRIPHONE
-                                              MOBILENO)
-     R1STAT.R1SDB&REPTMON&NOWK&REPTYEAR (KEEP=IC NAME ADDRESS PRIPHONE
-                                              MOBILENO);
-     SET SDBMS;
-RUN;
+09122025                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                            
+002 DAVID JAMES                                        00003                MY 00087      P BLOCK C 3-12F JALAN KENANGA SRI SARAWAK FLATS 55200 KL                                                                                                                                                                                                     N                      0321111111           0182154113           08042004 08042009 00000000 0 0 1 1 Hired                                                    
+002 DAVID JAMES                                        00003                MY 01968      P STARHILL DAAMAN SARA                                                                                                                                                                                                                                       N                      0321111111           0182154113           31012025 31012027 00000000 0 0 0 0 Hired                                                    
+002 DAVID JAMES                                        00003                MY 05017      P 3 PERSIARAN 12 47000 PUCHONG SELANGOR                                                                                                                                                                                                                      N                      0321111111           0182154113           10032020 31082024 00000000 0 0 1 1 Hired                                                    
+002 DAVID JAMES                                        00003                MY 50001      P NO 2, JLN LAZAT 2 TMN BANDARAYA 58200 KUALA LUMPUR                                                                                                                                                                                                         N 4993220706           0321111111           0182154113           31082016 20092024 00000000 0 0 1 1 Hired                                                    
+704 NG ZHI YONG                                        000505012022         MY 20004      P NO 1 JLN DUA TMN TIGA 43000 KAJANG SELANGOR                                                                                                     
