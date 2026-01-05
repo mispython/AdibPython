d5353efc5c07bbeb6371b7efbdfb7290029a4632
mispython <mis.saspython@gmail.com>
2026-01-05 17:05:10 +0800

    Update Amed.py

--- Amed.py
+++ Amed.py
@@ -1,8 +1,9 @@
-STG_LN_ACCTFILE.parquet
+Path	file
+/parquet/dwh/LOAN/staging	STG_LN_ACCTFILE.parquet
 STG_LN_ACC3FILE.parquet
-STG_DP_DPTRBLGS.parquet
-dpbtran*.sas7bdat
-STG_CIS_CISRLCC.parquet
-LKP_BRANCH
-enrh_exdwh_cisr1.sas7bdat
-LKP_CUSTCODE
+/parquet/dwh/DEPOSIT/staging	STG_DP_DPTRBLGS.parquet
+/dwh/cmsbdptr	dpbtran*.sas7bdat
+/parquet/dwh/CIS/staging	STG_CIS_CISRLCC.parquet
+/sasdata/rawdata/lookup	LKP_BRANCH
+/sas/cis/mart/enrichment	enrh_exdwh_cisr1.sas7bdat
+/sasdata/rawdata/lookup	LKP_CUSTCODE
