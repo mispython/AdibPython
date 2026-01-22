cd4531ba16de6f86e2a8d0b4ee9221bb1eb86f4a
mispython <mis.saspython@gmail.com>
2026-01-22 11:46:26 +0800

    Update A.py

--- A.py
+++ A.py
@@ -23,336 +23,335 @@ backports.tarfile==1.2.0
 backports.zstd==1.2.0
 bcrypt==4.2.1
 beautifulsoup4==4.13.4
-billiard==  4.2.4
-bleach==    6.2.0
-blinker==   1.9.0
-blis==      1.0.2
-bokeh==     3.4.3
+billiard==4.2.4
+bleach==6.2.0
+blinker==1.9.0
+blis==1.0.2
+bokeh==3.4.3
 Bottleneck==1.5.0
-brotli==    1.2.0
-cachelib==  0.13.0
+brotli==1.2.0
+cachelib==0.13.0
 cachetools==5.5.2
-catalogue== 2.0.10
-cattrs==    25.3.0
-celery==    5.6.0
-certifi==   2025.1.31
-cffi==      2.0.0
-chardet==   5.2.0
-charset-normalizer        3.4.1
-click==     8.1.8
-click-didyoumean          0.3.1
-click-option-group        0.5.9
+catalogue==2.0.10
+cattrs==25.3.0
+celery==5.6.0
+certifi==2025.1.31
+cffi==2.0.0
+chardet==5.2.0
+charset-normalizer==3.4.1
+click==8.1.8
+click-didyoumean==0.3.1
+click-option-group==0.5.9
 click-repl==0.3.0
-cloudpathlib              0.21.0
-cloudpickle               3.1.1
-coboljsonifier            1.0.8
+cloudpathlib==0.21.0
+cloudpickle==3.1.1
+coboljsonifier==1.0.8
 collection==0.1.6
-colorama==  0.4.6
-colorlog==  6.9.0
-comm==      0.2.3
+colorama==0.4.6
+colorlog==6.9.0
+comm==0.2.3
 confection==0.1.5
-conllu==    6.0.0
-contourpy== 1.3.0
-cron_descriptor           2.0.6
-cryptography              42.0.8
-cx-Oracle== 8.3.0
-cycler==    0.12.1
-cymem==     2.0.11
-dask==      2024.8.0
-dask-expr== 1.1.10
-debugpy==   1.8.14
-decorator== 5.2.1
+conllu==6.0.0
+contourpy==1.3.0
+cron_descriptor==2.0.6
+cryptography==42.0.8
+cx-Oracle==8.3.0
+cycler==0.12.1
+cymem==2.0.11
+dask==2024.8.0
+dask-expr==1.1.10
+debugpy==1.8.14
+decorator==5.2.1
 defusedxml==0.7.1
 Deprecated==1.3.1
-deprecation               2.1.0
-dill==      0.4.0
-distributed               2024.8.0
-dnspython== 2.7.0
-duckdb==    1.2.0
-ebcdic==    1.1.1
-ebcdic-parser             3.4.0
-email-validator           2.3.0
+deprecation==2.1.0
+dill==0.4.0
+distributed==2024.8.0
+dnspython==2.7.0
+duckdb==1.2.0
+ebcdic==1.1.1
+ebcdic-parser==3.4.0
+email-validator==2.3.0
 et_xmlfile==2.0.0
-exceptiongroup            1.3.1
-executing== 2.2.0
-fastjsonschema            2.21.2
-filelock==  3.19.1
-Flask==     2.3.3
-Flask-AppBuilder          4.8.1
-Flask-Babel               2.0.0
-Flask-Compress            1.23
-Flask-JWT-Extended        4.7.1
-Flask-Limiter             3.11.0
-Flask-Login               0.6.3
-Flask-Migrate             3.1.0
-Flask-Session             0.8.0
-Flask-SQLAlchemy          2.5.1
-flask-talisman            1.1.0
-Flask-WTF== 1.2.2
-fonttools== 4.56.0
-fpdf==      1.7.2
-fqdn==      1.5.1
-fs==        2.4.16
-fsspec==    2025.2.0
-fugue==     0.9.1
-func_timeout              4.3.5
-geographiclib             2.1
-geopy==     2.4.1
-google-auth               2.45.0
-graphviz==  0.21
-greenlet==  3.1.1
-gunicorn==  23.0.0
-h11==       0.16.0
-h5py==      3.13.0
-hashids==   1.3.1
-holidays==  0.25
-httpcore==  1.0.9
-httpx==     0.28.1
-humanize==  4.13.0
-ibm_db==    3.2.6
-idna==      3.10
-importlib_metadata        8.6.1
-importlib_resources       6.5.2
-imutils==   0.5.4
-iniconfig== 2.0.0
-ipykernel== 6.30.1
-ipython==   8.18.1
-isodate==   0.7.2
-isoduration               20.11.0
-itsdangerous              2.2.0
-jaraco.classes            3.4.0
-jaraco.context            6.1.0
-jaraco.functools          4.4.0
-jedi==      0.19.2
-jeepney==   0.9.0
-Jinja2==    3.1.5
-joblib==    1.4.2
-json5==     0.12.1
-jsonpath-ng               1.7.0
-jsonpointer               3.0.0
+exceptiongroup==1.3.1
+executing==2.2.0
+fastjsonschema==2.21.2
+filelock==3.19.1
+Flask==2.3.3
+Flask-AppBuilder==4.8.1
+Flask-Babel==2.0.0
+Flask-Compress==1.23
+Flask-JWT-Extended==4.7.1
+Flask-Limiter==3.11.0
+Flask-Login==0.6.3
+Flask-Migrate==3.1.0
+Flask-Session==0.8.0
+Flask-SQLAlchemy==2.5.1
+flask-talisman==1.1.0
+Flask-WTF==1.2.2
+fonttools==4.56.0
+fpdf==1.7.2
+fqdn==1.5.1
+fs==2.4.16
+fsspec==2025.2.0
+fugue==0.9.1
+func_timeout==4.3.5
+geographiclib==2.1
+geopy==2.4.1
+google-auth==2.45.0
+graphviz==0.21
+greenlet==3.1.1
+gunicorn==23.0.0
+h11==0.16.0
+h5py==3.13.0
+hashids==1.3.1
+holidays==0.25
+httpcore==1.0.9
+httpx==0.28.1
+humanize==4.13.0
+ibm_db==3.2.6
+idna==3.10
+importlib_metadata==8.6.1
+importlib_resources==6.5.2
+imutils==0.5.4
+iniconfig==2.0.0
+ipykernel==6.30.1
+ipython==8.18.1
+isodate==0.7.2
+isoduration==20.11.0
+itsdangerous==2.2.0
+jaraco.classes==3.4.0
+jaraco.context==6.1.0
+jaraco.functools==4.4.0
+jedi==0.19.2
+jeepney==0.9.0
+Jinja2==3.1.5
+joblib==1.4.2
+json5==0.12.1
+jsonpath-ng==1.7.0
+jsonpointer==3.0.0
 jsonschema==4.25.1
-jsonschema-specifications 2025.9.1
-jupyter_client            8.6.3
-jupyter_core              5.8.1
-jupyter-events            0.12.0
-jupyter-lsp               2.3.0
-jupyter_server            2.17.0
-jupyter_server_terminals  0.5.3
+jsonschema-specifications==2025.9.1
+jupyter_client==8.6.3
+jupyter_core==5.8.1
+jupyter-events==0.12.0
+jupyter-lsp==2.3.0
+jupyter_server==2.17.0
+jupyter_server_terminals==0.5.3
 jupyterlab==4.4.6
-jupyterlab_pygments       0.3.0
-jupyterlab_server         2.27.3
-keras==     3.9.0
-keyring==   25.7.0
-keyrings.alt              5.0.2
+jupyterlab_pygments==0.3.0
+jupyterlab_server==2.27.3
+keras==3.9.0
+keyring==25.7.0
+keyrings.alt==5.0.2
 kiwisolver==1.4.7
-kombu==     5.6.1
-korean-lunar-calendar     0.3.1
-langcodes== 3.5.0
-language_data             1.3.0
-lark==      1.2.2
-limits==    4.2
-llvmlite==  0.43.0
-locket==    1.0.0
-Mako==      1.3.10
-marisa-trie               1.2.1
-Markdown==  3.9
-markdown-it-py            3.0.0
+kombu==5.6.1
+korean-lunar-calendar==0.3.1
+langcodes==3.5.0
+language_data==1.3.0
+lark==1.2.2
+limits==4.2
+llvmlite==0.43.0
+locket==1.0.0
+Mako==1.3.10
+marisa-trie==1.2.1
+Markdown==3.9
+markdown-it-py==3.0.0
 MarkupSafe==3.0.2
-marshmallow               3.26.1
-marshmallow-sqlalchemy    1.4.2
+marshmallow==3.26.1
+marshmallow-sqlalchemy==1.4.2
 matplotlib==3.9.2
-matplotlib-inline         0.1.7
-mdurl==     0.1.2
-mimetypes-magic           0.4.30
-mistune==   3.1.4
-ml_dtypes== 0.5.1
-mmh3==      5.2.0
-more-itertools            10.8.0
-msgpack==   1.0.8
-msgspec==   0.20.0
-msrest==    0.7.1
-multiprocess              0.70.18
+matplotlib-inline==0.1.7
+mdurl==0.1.2
+mimetypes-magic==0.4.30
+mistune==3.1.4
+ml_dtypes==0.5.1
+mmh3==5.2.0
+more-itertools==10.8.0
+msgpack==1.0.8
+msgspec==0.20.0
+msrest==0.7.1
+multiprocess==0.70.18
 murmurhash==1.0.12
-namex==     0.0.8
-narwhals==  1.29.0
-nbclient==  0.10.2
-nbconvert== 7.16.6
-nbformat==  5.10.4
-nest-asyncio              1.6.0
-networkx==  3.2.1
-nh3==       0.2.22
-nmslib==    2.1.1
-notebook_shim             0.2.4
-numba==     0.60.0
-numexpr==   2.10.2
-numpy==     1.23.5
-oauthlib==  3.3.1
-opencv-python             4.11.0.86
-opencv-python-headless    4.11.0.86
-openpyxl==  3.1.5
-optree==    0.14.1
-oracledb==  3.1.1
-ordered-set               4.1.0
-outcome==   1.3.0.post0
-overrides== 7.7.0
-packaging== 24.2
-pandas==    2.2.3
-pandocfilters             1.5.1
-pantab==    5.2.2
-paramiko==  3.5.1
-parquetdb== 1.0.1
-parsedatetime             2.6
-parso==     0.8.4
-partd==     1.4.2
-pathos==    0.3.4
-patsy==     1.0.1
-pdf2image== 1.17.0
-pexpect==   4.9.0
-pgsanity==  0.3.0
-pillow==    11.1.0
-pip==       25.3
-platformdirs              4.3.8
-plotly==    6.0.0
-pluggy==    1.5.0
-ply==       3.11
-polars==    1.36.1
-polars-runtime-32         1.36.1
-polyline==  2.0.4
-pox==       0.3.6
-ppft==      1.7.7
-preshed==   3.0.9
-prison==    0.2.1
-prometheus_client         0.22.1
-prompt_toolkit            3.0.50
-protobuf==  6.32.1
-psutil==    7.0.0
+namex==0.0.8
+narwhals==1.29.0
+nbclient==0.10.2
+nbconvert==7.16.6
+nbformat==5.10.4
+nest-asyncio==1.6.0
+networkx==3.2.1
+nh3==0.2.22
+nmslib==2.1.1
+notebook_shim==0.2.4
+numba==0.60.0
+numexpr==2.10.2
+numpy==1.23.5
+oauthlib==3.3.1
+opencv-python==4.11.0.86
+opencv-python-headless==4.11.0.86
+openpyxl==3.1.5
+optree==0.14.1
+oracledb==3.1.1
+ordered-set==4.1.0
+outcome==1.3.0.post0
+overrides==7.7.0
+packaging==24.2
+pandas==2.2.3
+pandocfilters==1.5.1
+pantab==5.2.2
+paramiko==3.5.1
+parquetdb==1.0.1
+parsedatetime==2.6
+parso==0.8.4
+partd==1.4.2
+pathos==0.3.4
+patsy==1.0.1
+pdf2image==1.17.0
+pexpect==4.9.0
+pgsanity==0.3.0
+pillow==11.1.0
+pip==25.3
+platformdirs==4.3.8
+plotly==6.0.0
+pluggy==1.5.0
+ply==3.11
+polars==1.36.1
+polars-runtime-32==1.36.1
+polyline==2.0.4
+pox==0.3.6
+ppft==1.7.7
+preshed==3.0.9
+prison==0.2.1
+prometheus_client==0.22.1
+prompt_toolkit==3.0.50
+protobuf==6.32.1
+psutil==7.0.0
 ptyprocess==0.7.0
-pure_eval== 0.2.3
-pyarrow==   14.0.2
-pyasn1==    0.6.1
-pyasn1_modules            0.4.2
-pybind11==  2.6.1
-pycobol2csv               1.0.6
-pycparser== 2.22
-pycryptodome              3.23.0
-pydantic==  2.10.6
-pydantic_core             2.27.2
-pyglmnet==  1.1
-Pygments==  2.19.1
-pyiceberg== 0.9.1
-PyJWT==     2.10.1
-pymssql==   2.3.2
-PyMySQL==   1.1.1
-PyNaCl==    1.5.0
-pyodbc==    5.2.0
-pyparsing== 3.2.1
-pypdf==     5.3.0
-PyPDF2==    3.0.1
+pure_eval==0.2.3
+pyarrow==14.0.2
+pyasn1==0.6.1
+pyasn1_modules==0.4.2
+pybind11==2.6.1
+pycobol2csv==1.0.6
+pycparser==2.22
+pycryptodome==3.23.0
+pydantic==2.10.6
+pydantic_core==2.27.2
+pyglmnet==1.1
+Pygments==2.19.1
+pyiceberg==0.9.1
+PyJWT==2.10.1
+pymssql==2.3.2
+PyMySQL==1.1.1
+PyNaCl==1.5.0
+pyodbc==5.2.0
+pyparsing==3.2.1
+pypdf==5.3.0
+PyPDF2==3.0.1
 pyreadstat==1.2.8
-pysbd==     0.3.4
-PySocks==   1.7.1
-pyspnego==  0.11.2
-pytesseract               0.3.13
-pytest==    8.3.4
-python-crontab            3.3.0
-python-dateutil           2.9.0.post0
-python-dotenv             1.1.0
-python-json-logger        3.3.0
-pytz==      2025.1
-PyYAML==    6.0.2
-pyzmq==     27.1.0
-RapidFuzz== 3.13.0
-ray==       2.49.2
-redis==     4.6.0
-referencing               0.36.2
-requests==  2.32.3
-requests-cache            1.2.1
-requests_ntlm             1.3.0
-requests-oauthlib         2.0.0
-rfc3339-validator         0.1.4
-rfc3986-validator         0.1.1
-rfc3987-syntax            1.1.0
-rich==      13.9.4
-rpds-py==   0.27.1
-rsa==       4.9.1
-sas7bdat==  2.2.3
-saspy==     5.102.0
-scikit-learn              1.6.1
-scipy==     1.13.1
-scispacy==  0.5.5
-seaborn==   0.13.2
-SecretStorage             3.3.3
-selenium==  4.9.1
+pysbd==0.3.4
+PySocks==1.7.1
+pyspnego==0.11.2
+pytesseract==0.3.13
+pytest==8.3.4
+python-crontab==3.3.0
+python-dateutil==2.9.0.post0
+python-dotenv==1.1.0
+python-json-logger==3.3.0
+pytz==2025.1
+PyYAML==6.0.2
+pyzmq==27.1.0
+RapidFuzz==3.13.0
+ray==2.49.2
+redis==4.6.0
+referencing==0.36.2
+requests==2.32.3
+requests-cache==1.2.1
+requests_ntlm==1.3.0
+requests-oauthlib==2.0.0
+rfc3339-validator==0.1.4
+rfc3986-validator==0.1.1
+rfc3987-syntax==1.1.0
+rich==13.9.4
+rpds-py==0.27.1
+rsa==4.9.1
+sas7bdat==2.2.3
+saspy==5.102.0
+scikit-learn==1.6.1
+scipy==1.13.1
+scispacy==0.5.5
+seaborn==0.13.2
+SecretStorage==3.3.3
+selenium==4.9.1
 Send2Trash==1.8.3
 setuptools==80.9.0
-shellingham               1.5.4
+shellingham==1.5.4
 shillelagh==1.4.3
-shortid==   0.1.2
+shortid==0.1.2
 simplejson==3.20.1
-six==       1.17.0
-slack_sdk== 3.39.0
+six==1.17.0
+slack_sdk==3.39.0
 smart-open==7.1.0
-sniffio==   1.3.1
-sortedcontainers          2.4.0
-soupsieve== 2.7
-spacy==     3.7.5
-spacy-legacy              3.0.12
-spacy-loggers             1.0.5
+sniffio==1.3.1
+sortedcontainers==2.4.0
+soupsieve==2.7
+spacy==3.7.5
+spacy-legacy==3.0.12
+spacy-loggers==1.0.5
 SQLAlchemy==2.0.45
-SQLAlchemy-Utils          0.38.3
-sqlglot==   26.33.0
-sqlparse==  0.5.5
-srsly==     2.5.1
-sshtunnel== 0.4.0
+SQLAlchemy-Utils==0.38.3
+sqlglot==26.33.0
+sqlparse==0.5.5
+srsly==2.5.1
+sshtunnel==0.4.0
 stack-data==0.6.3
-statsmodels               0.14.4
+statsmodels==0.14.4
 strictyaml==1.7.3
-tableauserverclient       0.38
-tabula==    1.0.5
-tabulate==  0.8.10
-tblib==     3.0.0
-tenacity==  9.1.2
-terminado== 0.18.1
-Theano==    1.0.5
-thinc==     9.1.1
-threadpoolctl             3.5.0
-tinycss2==  1.4.0
-tk==        0.1.0
-toml==      0.10.2
-tomli==     2.2.1
-toolz==     1.0.0
-tornado==   6.4.2
-tqdm==      4.67.1
-traitlets== 5.14.3
-triad==     0.9.8
-trio==      0.31.0
-typer==     0.15.2
-types-python-dateutil     2.9.0.20250822
-typing==    3.7.4.3
-typing_extensions         4.15.0
-tzdata==    2025.3
-tzlocal==   5.3.1
-uri-template              1.3.0
-url-normalize             2.2.1
-urllib3==   2.3.0
-vaex==      4.17.0
+tableauserverclient==0.38
+tabula==1.0.5
+tabulate==0.8.10
+tblib==3.0.0
+tenacity==9.1.2
+terminado==0.18.1
+Theano==1.0.5
+thinc==9.1.1
+threadpoolctl==3.5.0
+tinycss2==1.4.0
+tk==0.1.0
+toml==0.10.2
+tomli==2.2.1
+toolz==1.0.0
+tornado==6.4.2
+tqdm==4.67.1
+traitlets==5.14.3
+triad==0.9.8
+trio==0.31.0
+typer==0.15.2
+types-python-dateutil==2.9.0.20250822
+typing==3.7.4.3
+typing_extensions==4.15.0
+tzdata==2025.3
+tzlocal==5.3.1
+uri-template==1.3.0
+url-normalize==2.2.1
+urllib3==2.3.0
+vaex==4.17.0
 variconfig==0.0.3
-vine==      5.1.0
-wasabi==    1.1.3
-wcwidth==   0.2.13
-weasel==    0.4.1
-webcolors== 24.11.1
-webencodings              0.5.1
-websocket-client          1.8.0
-Werkzeug==  3.1.4
-wheel==     0.45.1
-wrapt==     1.17.2
-wsproto==   1.2.0
-WTForms==   3.2.1
-WTForms-JSON              0.3.5
-xgboost==   2.1.4
+vine==5.1.0
+wasabi==1.1.3
+wcwidth==0.2.13
+weasel==0.4.1
+webcolors==24.11.1
+webencodings==0.5.1
+websocket-client==1.8.0
+Werkzeug==3.1.4
+wheel==0.45.1
+wrapt==1.17.2
+wsproto==1.2.0
+WTForms==3.2.1
+WTForms-JSON==0.3.5
+xgboost==2.1.4
 XlsxWriter==3.0.9
-xyzservices               2025.4.0
-zict==      3.0.0
-zipp==      3.21.0
-
+xyzservices==2025.4.0
+zict==3.0.0
+zipp==3.21.0
