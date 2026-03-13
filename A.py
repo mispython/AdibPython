436c59dde57f492ce958f4eea96ca85d923306ec
mispython <mis.saspython@gmail.com>
2026-03-13 10:09:31 +0800

    Update A.py

--- A.py
+++ A.py
@@ -1,5 +1,1 @@
-Hi Ms Puiling,
-I hope you are doing well.I am a Public Bank staff from the IT Division. I would like to seek clarification regarding the Staff Referral Campaign submission. I recently realized that I mistakenly used the previous Introducer's form LFOCT25 instead of LFJAN26 when submitting the referral form.However, three of my referred candidates have successfully accepted the job offer within the current campaign period (January 2026 – March 2026). I would like to confirm whether the use of the different form will affect my eligibility for the incentive payment.Additionally, may I know if there is any way to amend (in case it is not accepted) the previously submitted form so that the correct campaign form (LFJAN26) can be applied?I sincerely apologize for the oversight and would appreciate your guidance on how this matter can be rectified.Thank you for your assistance.Best regards,
-Adib Afham
-IT Division
-Public Bank
+
