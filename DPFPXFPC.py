18f01d88ac046b01c3b1687b4cbd619fe434435a
mispython <mis.saspython@gmail.com>
2026-06-16 12:24:58 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -3,3 +3,4 @@ EIBDDEPE - Daily Deposit Position Extract
 
 Reading DPTRBL Parquet file...
   Loaded 11,857,406 records from DPTRBL
+Error reading DPTRBL Parquet: 'DataFrame' object has no attribute 'first'
