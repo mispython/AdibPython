31231175c362dbaca95c847443eeccb747506bd7
mispython <mis.saspython@gmail.com>
2026-06-30 17:48:45 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -1,41 +1,19 @@
-Running EIBDCITX for 29/06/2026 (WK=4) - Processing YESTERDAY'S data
-================================================================================
-
-Loading input files...
-  ✓ Loaded DPFL: 89,493 rows from /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/DPFL.txt
-  ✓ Loaded EQFL: 421 rows from /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/UTSASDCID_20260629.txt
-  ✓ Loaded CRA: 1 rows from /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/DPCRATXT_20260629
-  Note: pyreadstat doesn't support column selection, loading full file then filtering...
-  ✓ Loaded EQRATE: 57 rows from /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/eqrate260629.sas7bdat
-  Loading MNITB Saving...
-  ✓ Loading from Parquet cache: /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/cache/intg_dp_acct_saving.parquet
-  ✓ Loaded MNITB Saving: 6,634,478 rows
-  Loading MNITB Current...
-  ✓ Loading from Parquet cache: /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/cache/intg_dp_acct_current.parquet
-  ✓ Loaded MNITB Current: 1,118,698 rows
-  Note: pyreadstat doesn't support column selection, loading full file then filtering...
-  ✓ Loaded DCID: 210 rows from /sas/python/virt_edw/Data_Warehouse/MIS/XMIS/input/prod/EIBDCITX/dcid0629.sas7bdat
-
-================================================================================
-
-Processing DPST...
-  DPST after merge: 89,493 rows
-
-Processing EQ data...
-  EQ after date filter: 300 rows
-  EQC: 150 rows, EQI: 150 rows
-
-Processing Customer Leg...
-  EQDCI after join: 5 rows
-  Note: No CRA records with valid status
-  DEPO combined: 7,753,176 rows
-  Note: No CRA or DEPO data to join
-  Combined EQDCI: 5 rows
-Traceback (most recent call last):
-  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBDCITX.py", line 916, in <module>
-    main()
-  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBDCITX.py", line 635, in main
-    eqrt_with_myr = pl.concat([eqrt, myr_row])
-  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/functions/eager.py", line 234, in concat
-    out = wrap_df(plr.concat_df(elems))
-polars.exceptions.ShapeError: unable to append to a DataFrame of width 7 with a DataFrame of width 2
+00000000: f1f5 f6e3 d4d9 e3d4 d961 c3d9 c1f0 f0f1  .........a......
+00000010: 61f0 f0f0 f0f0 f140 4040 4040 4040 4040  a......@@@@@@@@@
+00000020: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+00000030: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+00000040: 4040 0312 4483 820c e3c8 c5c9 40c3 c8c9  @@..D.......@...
+00000050: c5e6 40e8 d6d5 c740 4040 4040 4040 4040  ..@....@@@@@@@@@
+00000060: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+00000070: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+00000080: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+00000090: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+000000a0: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+000000b0: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+000000c0: 4040 4040 4040 4040 4040 4040 4040 4040  @@@@@@@@@@@@@@@@
+000000d0: 4040 4040 f6f5 f0f1 f2f1 f0f4 f5f4 f7f0  @@@@............
+000000e0: 4040 4040 4040 4040 0000 0000 0000 0000  @@@@@@@@........
+000000f0: 0000 0000 0000 0000 0000 0000 0000 0000  ................
+00000100: 0000 0000 0000 0000 0000 0000 0000 0000  ................
+00000110: 0000 0000 0000 0000 0000 0000 0000 0000  ................
+00000120: 0000 0000 0000 0000 0000 0000            ............
