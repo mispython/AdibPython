72fc2415a28e65fde0760ff023c87730d8904789
mispython <mis.saspython@gmail.com>
2026-07-17 15:52:32 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -1,4 +1,8 @@
-SC_GRADE = TRANSLATE(
-               TRANSLATE(SC_GRADE2, "[", "Ý"),
-               "]", "Û"
-           );
+lzopts servercp=iso8859-1,clientcp=ibm-1140,notrim,overflow=trunc    
+lzopts mode=text                                                     
+lzopts lrecl=134,recfm=fba,blksize=0,space=cyl.1.1                   
+cd /stgsrcsys/host/uat/kam/nna/output                                
+get LNFDBTAG_REPORT_20260715.txt    \                                
+                      //!RBD2.B033.SW.LNFDBTAG.TEST                  
+EOB                                                                  
+**************************** Bottom of Data *************************
