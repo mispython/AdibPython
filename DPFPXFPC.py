01a7df47b06a00e53a811f460f9e9e74890aef00
mispython <mis.saspython@gmail.com>
2026-08-02 17:04:44 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -1,4 +1,6 @@
 Traceback (most recent call last):
-  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBWCRMA.py", line 27, in <module>
-    OUT_BEP.mkdir(parents=True, exist_ok=True)
-AttributeError: 'str' object has no attribute 'mkdir'
+  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBWCRMA.py", line 77, in <module>
+    SAVING = pl.concat([read_sas7bdat(DEPO_SAV), read_sas7bdat(IDEPO_SAV)], how="vertical", rechunk=True)
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/functions/eager.py", line 234, in concat
+    out = wrap_df(plr.concat_df(elems))
+polars.exceptions.ShapeError: unable to append to a DataFrame of width 86 with a DataFrame of width 88
