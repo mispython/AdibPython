3f80ec907c31b4eefdfa1d2f4a70a307c29aa0df
mispython <mis.saspython@gmail.com>
2026-08-18 10:20:01 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -1,98 +1,51 @@
-# =========================================================
-# 11. PROCESS BTR2
-# =========================================================
-print("\nProcessing BTR2...")
+Reading input files...
+  Read imast: 1697 rows
+  Read imast2: 2246 rows
+  Read icred: 10414 rows
+  Read isuba: 61339 rows
+  Read iprov: 87 rows
+  Read iamsubacc: 0 rows
+  Read ibtrad: 3418 rows
+  Read ibtdtl: 3418 rows
+  Read lnacct: 1205962 rows
 
-btr2 = None
-btr2x = None
-btr3a = None
+Processing MAST...
+MAST processed: 1697 rows
 
-if cred is not None and suba_main is not None:
-    btr2 = cred.join(suba_main, on=["acctnox", "transref"], how="inner")
-    
-    # Fix column names and add calculated fields
-    btr2 = btr2.with_columns([
-        # Apply special facility mapping for UTRDF='R'
-        pl.when(
-            (pl.col("utrdf").cast(pl.Utf8) == 'R') & 
-            (pl.col("liabcode").cast(pl.Utf8).is_in(['BAE', 'BEI']))
-        ).then(pl.lit("34471"))
-        .when(
-            (pl.col("utrdf").cast(pl.Utf8) == 'R') & 
-            (pl.col("liabcode").cast(pl.Utf8).is_in(['BAI', 'BII']))
-        ).then(pl.lit("34472"))
-        .when(
-            (pl.col("utrdf").cast(pl.Utf8) == 'R') & 
-            (pl.col("liabcode").cast(pl.Utf8).is_in(['BAP', 'BAS', 'BPI', 'BSI']))
-        ).then(pl.lit("34475"))
-        .otherwise(pl.col("facility"))
-        .alias("facility"),
-        
-        # TFR02I flag (cast to string first for comparison)
-        pl.when(pl.col("tfindr02").cast(pl.Utf8) == "5").then(1).otherwise(0).alias("tfr02i"),
-        
-        # PDBIND flag
-        pl.when(pl.col("subprod").cast(pl.Utf8) == "PDB-I")
-          .then(pl.lit("Y"))
-          .otherwise(pl.lit("N"))
-          .alias("pdbind"),
-        
-        # SPECIALF handling (cast to string for comparison)
-        pl.when(pl.col("specialf").cast(pl.Utf8).is_in(['20', '25', '30']))
-          .then(1)
-          .otherwise(0)
-          .alias("sfs"),
-        pl.when(pl.col("specialf").cast(pl.Utf8).is_in(['20', '25', '30']))
-          .then(0)
-          .otherwise(1)
-          .alias("nonsfs"),
-        
-        # Reset NODAYS if OUTSTAND is 0
-        pl.when(
-            (pl.col("nodays") > 0) & (pl.col("outstand").cast(pl.Float64, strict=False) < 1)
-        ).then(0).otherwise(pl.col("nodays")).alias("nodays"),
-        pl.when(
-            (pl.col("nodays") > 0) & (pl.col("outstand").cast(pl.Float64, strict=False) < 1)
-        ).then(0).otherwise(pl.col("arrears")).alias("arrears"),
-        pl.when(
-            (pl.col("nodays") > 0) & (pl.col("outstand").cast(pl.Float64, strict=False) < 1)
-        ).then(0).otherwise(pl.col("instalm")).alias("instalm")
-    ])
-    
-    # Handle PRODGRP = 'BA' separately
-    prodgrp_col = 'prodgrp' if 'prodgrp' in btr2.columns else 'prodgrp_right'
-    if prodgrp_col in btr2.columns:
-        btr2 = btr2.with_columns([
-            pl.when(pl.col(prodgrp_col).cast(pl.Utf8) == 'BA')
-              .then(pl.col("balance"))
-              .otherwise(pl.lit(None))
-              .alias("prinamt_myrx_ba"),
-            pl.when(pl.col(prodgrp_col).cast(pl.Utf8) == 'BA')
-              .then(pl.col("unearned"))
-              .otherwise(pl.lit(None))
-              .alias("intamt_myrx_ba")
-        ])
-    
-    # Summarize BTR2
-    btr3a = btr2.group_by(["acctnox", "facility", "forcurr", "pdbind"]).agg([
-        pl.col("outstand").sum().alias("outstand"),
-        pl.col("instalm").sum().alias("instalm"),
-        pl.col("unearned").sum().alias("unearned"),
-        pl.col("repaid").sum().alias("repaid"),
-        pl.col("disburse").sum().alias("disburse"),
-        pl.col("tfr02i").sum().alias("tfr02i"),
-        pl.col("mtd_tawidh_amt").sum().alias("mtd_tawidh_amt"),
-        pl.col("mtd_gharamah_amt").sum().alias("mtd_gharamah_amt"),
-        pl.col("prinamt_myrx").sum().alias("prinamt_myrx"),
-        pl.col("intamt_myrx").sum().alias("intamt_myrx"),
-        pl.col("oth_chargex").sum().alias("oth_chargex"),
-        pl.col("nodays").max().alias("nodays")
-    ])
-    
-    # Get max NODAYS per account
-    btr2x = btr2.sort(
-        ["acctnox", "facility", "forcurr", "pdbind", "nodays"],
-        descending=[False, False, False, False, True]
-    ).unique(subset=["acctnox", "facility", "forcurr", "pdbind"], keep="first")
-    
-    print(f"BTR2 processed: {btr2.height} rows")
+Processing MAST2...
+MAST2 processed
+
+Processing CRED...
+CRED processed: 3419 rows
+
+Processing BNM Trade data...
+BNM Trade data processed
+
+Processing SUBA...
+SUBA processed: 61339 rows (SUBA9: 1184, SUBA_MAIN: 10411)
+
+Processing ACCT...
+ACCT processed: 1644 rows
+
+Processing BTR2...
+BTR2 processed: 10411 rows
+
+Processing SUBCR...
+Traceback (most recent call last):
+  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIIWBTCR_ISLAMIC_WEEKLY_BANKTRADE_CCR.py", line 610, in <module>
+    subcr = btr2x.join(btr3a, on=["acctnox", "facility", "forcurr", "pdbind"], how="inner")
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/_utils/deprecation.py", line 128, in wrapper
+    return function(*args, **kwargs)
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/dataframe/frame.py", line 8242, in join
+    self.lazy()
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/_utils/deprecation.py", line 97, in wrapper
+    return function(*args, **kwargs)
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/lazyframe/opt_flags.py", line 328, in wrapper
+    return function(*args, **kwargs)
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/lazyframe/frame.py", line 2429, in collect
+    return wrap_df(ldf.collect(engine, callback))
+polars.exceptions.DuplicateError: column with name 'mtd_tawidh_amt_right' already exists
+
+You may want to try:
+- renaming the column prior to joining
+- using the `suffix` parameter to specify a suffix different to the default one ('_right')
