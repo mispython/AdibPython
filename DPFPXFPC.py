ea444bbd2217aafc3d567fe25ba56b8c2d3f184a
mispython <mis.saspython@gmail.com>
2026-08-23 18:21:02 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -1,7 +1,11 @@
-REPTMON: 08, RDATE: 220826
-============================================================
-PUBLIC BANK BERHAD
-DETAIL OF ACCTS (MEF PRODUCTS) FOR SUBMISSION TO CGC @ 220826
-============================================================
-MEFT.txt file created with 0 records
-Report saved to MEFR.txt
+REPTMON: 07, RDATE: 310726
+Traceback (most recent call last):
+  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBRP159.py", line 179, in <module>
+    eibrp159()
+  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBRP159.py", line 49, in eibrp159
+    npgs_df = pl.concat([dp_df, ln_df], how="vertical")
+  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/functions/eager.py", line 234, in concat
+    out = wrap_df(plr.concat_df(elems))
+polars.exceptions.ShapeError: unable to vstack, column names don't match: "cvar13" and "product"
+
+i tried change the timedelta(days=23) to use july's data input
