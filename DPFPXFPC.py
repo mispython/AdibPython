d1910c215186d09c4e6cf2fcbcd9fa19d951d1e7
mispython <mis.saspython@gmail.com>
2026-09-09 19:04:31 +0800

    Update DPFPXFPC.py

--- DPFPXFPC.py
+++ DPFPXFPC.py
@@ -1,19 +1,3 @@
-Step 1: Setting report date...
-Report Date: 2026-09-08 19:00:31.845079, RDATE: 24357
-Step 2: Processing credit facility table...
-Step 3: Merging with master account data...
-Step 4: Processing credit data...
-Traceback (most recent call last):
-  File "/sas/python/virt_edw/Data_Warehouse/MIS/XMIS/EIBTNPGS.py", line 191, in <module>
-    cred_data = cred_data.join(crft_final, on=['ACCTNO', 'SUBACCT'], how='inner')
-  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/_utils/deprecation.py", line 128, in wrapper
-    return function(*args, **kwargs)
-  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/dataframe/frame.py", line 8242, in join
-    self.lazy()
-  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/_utils/deprecation.py", line 97, in wrapper
-    return function(*args, **kwargs)
-  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/lazyframe/opt_flags.py", line 328, in wrapper
-    return function(*args, **kwargs)
-  File "/sas/python/virt_edw_dev/lib64/python3.9/site-packages/polars/lazyframe/frame.py", line 2429, in collect
-    return wrap_df(ldf.collect(engine, callback))
-polars.exceptions.SchemaError: datatypes of join keys don't match - `ACCTNO`: f64 on left does not match `ACCTNO`: i64 on right (and no other type was available to cast to)
+PD	FAC2	Y092164		2	BT	2500003902	00000	260908	123000	908.56	0	0	0		PBA	V	365	908.56	123000	0	0	2500003902
+PD	OV	Y092164		2	BT	2500003902	00000	260908	123000	908.56	0	0	0		PBA	V	365	908.56	123000	0	0	2500003902
+PD	2BAX2	Y092164		2	BT	2500003902	34470	260908	123000	908.56	0	0	0		PBA	V	365	908.56	123000	0	0	2500003902
